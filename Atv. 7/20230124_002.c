69485db886b2d133f72a58ed1743bf8e1104b00e
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-02-08 11:02:55 -0400

    Update 20230124_002.c

--- Atv. 7/20230124_002.c
+++ Atv. 7/20230124_002.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define TAMANHO 100
+#define M 100
 
-void imprimirInversa(int v[], int tamanho){
-    int i;
-    for (i=tamanho-1; i>=0; i--){
-        printf(" %d", v[i]);
+void imprimir(){
+    int seq[M], qtde;
+    printf("Digite  a quantidade de numeros: ");
+    scanf("%d", &qtde);
+    for(int i=0; i<qtde; i++){
+        printf("Digite o numero: ");
+        scanf("%d", &seq[i]);
+    }
+    printf("A sequencia inversa e: ");
+    for(int j=qtde-1; j>=0; j--){
+        printf("%d", seq[j]);
     }
 }
 
 int main(){
-    int v[TAMANHO];
-    int num;
-    int i;
-    printf ("Quantidade de numeros: ");
-    scanf("%d",&num);
-    for (i=0; i < num; i++){
-        printf("Digite o %d numero: ", i+1);
-        scanf ("%d", &v[i]);
-    }
-    imprimirInversa(v,num);
-    return 0;
+    imprimir();
 }
