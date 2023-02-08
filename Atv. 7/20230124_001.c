fe40b42399e8d8bec1be43d85edeffed9e4fd2fe
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-02-08 10:53:05 -0400

    Create 20230124_001.c

---
+++ Atv. 7/20230124_001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define M 100
+
+int main(){
+    int seq[M], qtde;
+    printf("Digite  a quantidade de numeros: ");
+    scanf("%d", &qtde);
+    for(int i=0; i<qtde; i++){
+        printf("Digite o numero: ");
+        scanf("%d", &seq[i]);
+    }
+    printf("A sequencia inversa e: ");
+    for(int j=qtde-1; j>=0; j--){
+        printf("%d", seq[j]);
+    }
+}
