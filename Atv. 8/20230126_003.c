6277b05dceabbf7b12d42f29af41e3a540f2f690
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-02-09 12:04:16 -0400

    Update 20230126_003.c

--- Atv. 8/20230126_003.c
+++ Atv. 8/20230126_003.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 100
 
 int main(){
-    int linha = 3, coluna = 5, k, produto;
-    int m1[MAX][MAX]={{1,1,1,1,1},{2,2,2,2,2},{3,3,3,3,3}};
-    printf("Digite um numero escalar qualquer: ");
-    scanf("%d", k);
+    int matriz[3][3];
+    int k, multi;
     
-    for (int i = 0; i < linha; i++){
-        for (int j = 0; j < coluna; j++){
-            produto = k * m1[i][j];
-            printf ("resultado[%d][%d] = %d\n", i, j, produto);
+    for(int ln1=0; ln1<3; ln1++){
+        for(int cl1=0; cl1<3; cl1++){
+            printf("Digite um numero para a matriz: ");
+            scanf("%d", &matriz[ln1][cl1]);
+        }
+    }
+    
+    printf("Digite um numero escalar: ");
+    scanf("%d", &k);
+    
+    for(int ln2=0; ln2<3; ln2++){
+        for(int cl2=0; cl2<3; cl2++){
+            multi = matriz[ln2][cl2] * k;
+            printf("As multiplicacoes sao: %d\n", multi);
         }
     }
 }
