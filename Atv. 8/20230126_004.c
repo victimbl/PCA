d4bd1ce1eaeaf535bed7ec5c9fe8a4cfec76a653
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-02-13 11:30:46 -0400

    Create 20230126_004.c

---
+++ Atv. 8/20230126_004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int matriz[3][3], x;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            printf("Digite os numeros da matriz: ");
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    printf("Digite um valor qualquer: ");
+    scanf("%d", &x);
+    for()
+}
