72e38d4591859c09783ae24d8d143ac3625f23dd
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-02-14 15:40:32 -0400

    Update 20230131_001.c

--- Atv. 9/20230131_001.c
+++ Atv. 9/20230131_001.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#define max 81
 
-void inversa(char p[]){
-    int copia, fim, tam;
-    tam = strlen(p);
-    fim = tam - 1;
-    for(int i=0; i<(tam/2); i++){
-        copia = p[i];
-        p[i] = p[fim];
-        p[fim] = copia;
-        fim--;
+void inverter(char palavra[max]){
+    char cpy[max];
+    int tam = strlen(palavra) - 1;
+    int i;
+    for(i=0; palavra[i]!='\0'; i++){
+        cpy[tam] = palavra[i];
+        tam--;
     }
+    cpy[i] = '\0';
+    printf("A palavra invertida e: %s\n", cpy);
+}
+
+void semvogal(char palavra[max]){
+    char svogal[max];
+    int i, j=0, tam = strlen(palavra);
+    for(i=0; palavra[i]!='\0'; i++){
+        if(palavra[i]!='a' && palavra[i]!='e' && palavra[i]!='i' && palavra[i]!='o' && palavra[i]!='u' && 
+           palavra[i]!='A' && palavra[i]!='E' && palavra[i]!='I' && palavra[i]!='O' && palavra[i]!='U'){
+            svogal[j] = palavra[i];
+            j++;
+        }
+    }
+    printf("A palavra sem vogais e: ");
+    puts(svogal);
 }
 
 int main(){
-    char palavra[] = "Tamanho";
-    inversa(palavra);
-    printf("%s", palavra);
+    char palavra[max];
+    printf("Digite a palavra que sera usada: ");
+    gets(palavra);
+    inverter(palavra);
+    semvogal(palavra);
 }
