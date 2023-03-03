6b8731b2f642357867317c0745fb330232bcb1a8
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-03-03 14:59:57 -0400

    Create 20222220006.c

---
+++ Atv. 3/20222220006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int prim, seg, tab_multi;
+    printf("A tabuada de multiplicacao: ");
+    for(prim=1; prim<10; prim++){
+        printf("\n");
+        for(seg=1; seg<10; seg++){
+            tab_multi = prim * seg;
+            printf("%d x %d = %d\n", prim, seg, tab_multi);
+        }
+    }
+}
